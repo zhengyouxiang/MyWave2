57f7b17882a461ea23c5dd03ca83d169bb9e8bdb
youngsing <youngsingyx@gmail.com>
2013-03-07 12:58:43 +0800

    Initial Commit

---
+++ MyWave2/YSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSAppDelegate.h
+//  MyWave2
+//
+//  Created by youngsing on 13-3-7.
+//  Copyright (c) 2013年 youngsing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyWave2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyWave2
+//
+//  Created by youngsing on 13-3-7.
+//  Copyright (c) 2013年 youngsing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YSAppDelegate class]));
+    }
+}
